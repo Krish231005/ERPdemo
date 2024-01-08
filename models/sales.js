aafaa9feefdb6e0f6980f3abf7e98224e55866d0
Krish231005 <155898703+Krish231005@users.noreply.github.com>
2024-01-08 11:39:00 +0530

    sales update

---
+++ models/sales.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const salesSchema = new mongoose.Schema({
+  product: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  date: { type: Date, default: Date.now },
+});
+
+const Sales = mongoose.model('Sales', salesSchema);
+
+module.exports = Sales;
