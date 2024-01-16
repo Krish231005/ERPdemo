18108564e67f72e8868c1ef29f8d50683758f20c
Krish231005 <155898703+Krish231005@users.noreply.github.com>
2024-01-16 11:13:07 +0530

    5

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const mongooseConnection = require('./db/connection');
-const productRoutes = require('./routes/productRoutes'); 
-const userRoutes = require('./routes/userRoutes');
-const bodyParser = require('body-parser');
-const salesRoutes = require('./routes/salesRoutes');
+const api = require('./api/allApi');
 const app = express();
-app.use(bodyParser.json());
-app.use('/products', productRoutes);
-app.use('/users', userRoutes);
-app.use('/sales', salesRoutes);
+
 mongooseConnection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 mongooseConnection.once('open', () => {
   console.log('Connected to MongoDB');
-  
+  app.use('/api',api);
   const port = 3000;
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
